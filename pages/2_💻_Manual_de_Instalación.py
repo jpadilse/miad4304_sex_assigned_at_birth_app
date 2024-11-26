769c5496bac1dc72ff6c2ef017755821fcf3a458
jupadill_nequi <jupadill@nequi.com>
2024-11-26 15:33:22 -0500

    Fixed: installation manual

--- pages/2_💻_Manual_de_Instalación.py
+++ pages/2_💻_Manual_de_Instalación.py
@@ -1,23 +1,22 @@
 import streamlit as st
 
-# Set page configuration
+# Configuración de la página
 st.set_page_config(page_title="Manual de Instalación", page_icon="💻")
 
-# Page title
-st.title("👩Predicción de género por nombre👨")
-st.header("📖 Manual de Instalación 📖")
+# Título de la página
+st.title("👩 Predicción de Género por Nombre 👨")
+st.header("📖 Manual de Instalación")
 
-# Introduction
+# Introducción
 st.subheader("📗 Introducción")
 st.write(
 		"""
-		Este manual detalla los pasos necesarios para instalar y ejecutar un proyecto de predicción de género por nombres basado en Python y Streamlit. 
-		El proyecto está diseñado para ser ejecutado en un entorno AWS. Este documento también incluye soluciones a problemas comunes y configuraciones específicas 
-		para garantizar un correcto despliegue.
+		Este manual describe los pasos para instalar y ejecutar un proyecto de predicción de género por nombres usando Python y Streamlit 
+    	en un entorno AWS.
 		"""
 )
 
-# AWS Preparation
+# Preparación de AWS
 st.subheader("📘 Preparación Instancia AWS")
 
 st.write("Antes de iniciar, asegúrate de contar con:")
@@ -33,74 +32,35 @@ st.markdown(
 		"""
 )
 
-st.write("#### Pasos para configurar la instancia:")
-st.markdown(
-		"""
-		1. Accede a la consola de AWS, selecciona *Lanzar Instancia* y elige Ubuntu como sistema operativo.
-		2. Configura el tipo de instancia como **t2.small** o superior.
-		3. Asigna **20 GB** de almacenamiento.
-		4. Agrega reglas para permitir el tráfico en los puertos mencionados:
-		   - **22**: Acceso SSH desde tu IP.
-		   - **8501**: Acceso HTTP desde tu IP pública (o abierta temporalmente).
-		5. Usa la llave SSH descargada al crear la instancia. Conéctate desde la terminal con el siguiente comando:
-		```bash
-		ssh -i llave.pem ubuntu@<IP>
-		```
-		6. Una vez dentro de la instancia, actualiza los paquetes:
-		```bash
-		sudo apt update && sudo apt upgrade -y
-		```
-		"""
-)
-
-# Installation steps
+# Pasos de instalación
 st.subheader("📙 Instalación")
 
 st.markdown(
 		"""
-		1. Clona el proyecto desde GitHub y accede al directorio:
-		```bash
-		git clone https://github.com/jpadilse/miad4304_sex_assigned_at_birth_ds.git
-		```
-	
-		2. Accede a la carpeta del proyecto:
-		```bash
-		cd miad4304_sex_assigned_at_birth_ds
-		```
-	
-		3. Crea un entorno virtual y actívalo:
-		```bash
-		python3 -m venv venv
-		source venv/bin/activate
-		```
-	
-		4. Instala los paquetes listados en el archivo `requirements.txt`:
-		```bash
-		pip install -r requirements.txt
-		```
-	
-		5. Inicia la aplicación del proyecto con Streamlit:
-		```bash
-		streamlit run app.py --server.port 8501
-		```
-	
-		6. Abre tu navegador y accede a la URL correspondiente:
+		1. Clonar el repositorio:
+		   ```bash
+		   git clone https://github.com/jpadilse/miad4304_sex_assigned_at_birth_app.git
+		   cd miad4304_sex_assigned_at_birth_app
+		   ```
+		
+		2. Crear un entorno virtual (opcional pero recomendado):
+		   ```bash
+		   python -m venv venv
+		   source venv/bin/activate 
+		   ```
+		
+		3. Instalar dependencias:
+		   ```bash
+		   pip install -r requirements.txt
+		   ```
+		
+		4. Inicia la aplicación del proyecto con Streamlit:
+		   ```bash
+		   streamlit run app.py --server.port 8501
+		   ```
+		
+		5. Abre tu navegador y accede a la URL correspondiente:
 		   - **Localmente**: [http://localhost:8501](http://localhost:8501).
-		   - **En AWS**: [http://<IP-pública-de-tu-instancia>:8501](http://<IP-pública-de-tu-instancia>:8501).
+		   - **Online**: [http://<IP-pública-de-tu-instancia>:8501](http://<IP-pública-de-tu-instancia>:8501).
 		"""
 )
-
-# FAQs
-st.subheader("🎯 Preguntas Frecuentes")
-
-with st.expander("❓ ¿Dónde puedo comunicarme si tengo algún inconveniente?"):
-	st.write(
-			"""
-			Puedes comunicarte con alguno de los miembros del equipo responsable del desarrollo de la herramienta, enviando un correo a:
-	
-			- 😊 j.padillas@uniandes.edu.co
-			- 😊 ek.diaz@uniandes.edu.co
-			- 😊 a.quinoneso@uniandes.edu.co
-			- 😊 m.gonzalezc@uniandes.edu.co
-			"""
-	)
