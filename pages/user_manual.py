a8fea0e0464e79e30628afb4f44c0e3359ea1176
jupadill_nequi <jupadill@nequi.com>
2024-11-26 12:43:48 -0500

    Feature: user manual

---
+++ pages/user_manual.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Manual de Usuario", page_icon="📈")
+
+st.markdown("# Plotting Demo 1")
+st.sidebar.header("Plotting Demo 2")
+st.write(
+		"""This demo illustrates a combination of plotting and animation with
+	Streamlit. We're generating a bunch of random numbers in a loop for around
+	5 seconds. Enjoy!"""
+)
